54e416eea1f7eaeeb527adecb0259dce6df49d53
princeofdarkness76  script type="text/javascript" src="https://cdn.ywxi.net/js/1.js" async/script <djsteviejames@hotmail.com>
2015-12-13 22:50:35 -0800

    Merge remote-tracking branch 'origin/devel_ble_support' into pod/greentea-mbed-usb-device
    # Conflicts:
    #	mbed_greentea/mbed_greentea_cli.py
    #	setup.py

--- mbed_greentea/mbed_greentea_cli.py
+++ mbed_greentea/mbed_greentea_cli.py
@@ -70,6 +70,7 @@ from mbed_greentea_dlm import greentea_get_app_sem
 from mbed_greentea_dlm import greentea_update_kettle
 from mbed_greentea_dlm import greentea_clean_kettle
 <<<<<<< HEAD
+<<<<<<< HEAD
 from mbed_greentea_dlm import greentea_kettle_info
 from mbed_greentea_dlm import greentea_release_target_id
 from mbed_greentea_dlm import greentea_acquire_target_id_from_list
@@ -93,6 +94,12 @@ from mbedgt_meshtest import main_meshtest_cli
 from mbedgt_singletest import main_singletest_cli
 
 >>>>>>> ARMmbed/devel_ble_support
+=======
+
+from mbedgt_meshtest import main_meshtest_cli
+from mbedgt_singletest import main_singletest_cli
+
+>>>>>>> origin/devel_ble_support
 
 try:
     import mbed_lstools
@@ -103,6 +110,7 @@ except ImportError as e:
 MBED_LMTOOLS = 'mbed_lstools' in sys.modules
 MBED_HOST_TESTS = 'mbed_host_tests' in sys.modules
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -135,6 +143,11 @@ def print_version(verbose=True):
 def main():
     """ Closure for main_cli() function """
 =======
+=======
+
+def main():
+    """ Closure for main_singletest_cli() function """
+>>>>>>> origin/devel_ble_support
 
 def main():
 =======
@@ -310,14 +323,18 @@ def main():
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> origin/devel_ble_support
 =======
 >>>>>>> ARMmbed/devel_ble_support
+=======
+>>>>>>> origin/devel_ble_support
 
     if not MBED_HOST_TESTS:
         gt_log_err("error: mbed-host-tests proprietary module not installed")
         exit(-1)
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 
@@ -346,6 +363,15 @@ def main():
         main_cli = main_singletest_cli
 
 >>>>>>> ARMmbed/devel_ble_support
+=======
+
+    # Select which functionality will drive CLI
+    if opts.mesh_test_module:
+        main_cli = main_meshtest_cli
+    else:
+        main_cli = main_singletest_cli
+
+>>>>>>> origin/devel_ble_support
     cli_ret = 0
     start = time()
     if opts.lock_by_target:
@@ -412,6 +438,7 @@ def main():
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> ARMmbed/devel_ble_support
 =======
 >>>>>>> origin/devel_ble_support
@@ -419,6 +446,8 @@ def main():
 >>>>>>> origin/devel_ble_support
 =======
 >>>>>>> ARMmbed/devel_ble_support
+=======
+>>>>>>> origin/devel_ble_support
             gt_log_tab(str(e))
             raise
 
